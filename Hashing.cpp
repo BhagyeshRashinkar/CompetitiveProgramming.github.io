8460058c7b3771ac42ccc8a5ca624502d30d6111
BhagyeshRashinkar <99868437+BhagyeshRashinkar@users.noreply.github.com>
2023-02-11 20:39:05 +0530

    added Hashing

---
+++ Hashing.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+
+    vector<int> hash(13,0);
+    for(int i=0; i<n; i++){
+        hash[a[i]] += 1;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        cout<<hash[num]<<nl;
+    }
+
+    return 0;
+}
