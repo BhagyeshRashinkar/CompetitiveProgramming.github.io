c784aaf0f5b792c2d64bc1c88b374a0cc9785cee
BhagyeshRashinkar <99868437+BhagyeshRashinkar@users.noreply.github.com>
2023-02-11 20:58:07 +0530

    hashing and kadane

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 #define ll long long int
 #define nl '\n'
@@ -27,34 +26,4 @@ int main()
     }
 
     return 0;
-=======
-#include <bits/stdc++.h>
-#define ll long long int
-#define nl '\n'
-using namespace std;
-
-int main()
-{
-    int n;
-    cin>>n;
-    int a[n];
-    for(int i=0; i<n; i++){
-        cin>>a[i];
-    }
-
-    vector<int> hash(13,0);
-    for(int i=0; i<n; i++){
-        hash[a[i]] += 1;
-    }
-
-    int q;
-    cin>>q;
-    while(q--){
-        int num;
-        cin>>num;
-        cout<<hash[num]<<nl;
-    }
-
-    return 0;
->>>>>>> e39179e06d66c50604cfbfcd2dda670525760d20
 }
